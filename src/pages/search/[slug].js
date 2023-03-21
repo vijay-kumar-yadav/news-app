cddec4ae40439219e038e0357d2020088cc63d54
vijay-kumar-yadav <vky405021@gmail.com>
2023-03-22 01:00:08 +0530

    api test

--- src/pages/search/[slug].js
+++ src/pages/search/[slug].js
@@ -50,7 +50,8 @@ export const Search = ({ pageNumber }) => {
         },
         headers: {
           "X-BingApis-SDK": "true",
-          "X-RapidAPI-Key": process.env.NEXTJS_PUBLIC_NEWS_KEY,
+          "X-RapidAPI-Key":
+            "bfc8ff3d12msh8a317fb7700949cp19e0bajsn2f1b89041e68",
           "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
         },
       };
